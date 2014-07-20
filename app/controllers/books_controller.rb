3f857229fde14e6403d988ee2eb440fc2a276ba2
p1nox <pinox101@gmail.com>
2014-07-20 09:38:31 -0400

    include book list on response and reset application controller protect_from_forgery commented

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,7 @@ class BooksController < ApplicationController
   def to_read_before_death
     @age            = request.parameters['age'].to_f
     @books_per_year = request.parameters['books_per_year'].to_f
-    @country_name   = request.location.data['country_name']
+    @country_name   = request.location.data['country_name'].downcase
 
     country          = TheCountryIdentity::RDF.new @country_name
     @life_expectancy = country.get_property 'lifeexpectancyatbirth_totalpopulation'
@@ -17,12 +17,16 @@ class BooksController < ApplicationController
 
     @books_to_read = ( ( @life_expectancy - @age ) * @books_per_year ).round(2)
 
+    books_count = if @books_to_read <= 1001 then @books_to_read else 1001 end
+    @books_list = Book.get_random_books_list books_count
+
     render json: {
-      'books_to_read' => @books_to_read,
       'age' => @age,
       'books_per_year' => @books_per_year,
       'country_name' => @country_name,
-      'life_expectancy' => @life_expectancy
+      'life_expectancy' => @life_expectancy,
+      'books_to_read' => @books_to_read,
+      'books_list' => @books_list
     }
   end
 
--- test/controllers/books_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class BooksControllerTest < ActionController::TestCase
-  test "should get to_read_before_death" do
-    get :to_read_before_death
-    assert_response :success
-  end
-
-end
